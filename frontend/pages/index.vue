eeb25c25b11d45193983d238237d67e8ae62e70b
homuncus <cot.cotenov@gmail.com>
2023-07-28 14:17:34 +0300

    [update] todo view

---
+++ frontend/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <Todo v-for="todo in todos" :todo="todo" />
+</template>
+
+<script lang="ts" setup>
+definePageMeta({
+  middleware: 'auth'
+})
+
+import { todoService } from 'services'
+import type { Todo } from 'project-types'
+
+const todos = ref<Todo[]>([])
+
+onMounted(async () => {
+  todos.value = await todoService.getAll()
+})
+</script>
